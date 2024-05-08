593bf6f521254062d08de4f934d571a96dd51ae7
Mookhyang1131 <151420951+Mookhyang1131@users.noreply.github.com>
2024-05-08 23:21:36 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,19 +1,5 @@
-#    This file is part of the ChannelAutoForwarder distribution (https://github.com/xditya/ChannelAutoForwarder).
-#    Copyright (c) 2021-2022 Aditya
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, version 3.
-#
-#    This program is distributed in the hope that it will be useful, but
-#    WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-#    General Public License for more details.
-#
-#    License can be found in < https://github.com/xditya/ChannelAutoForwarder/blob/main/License> .
-
 import logging
-from telethon import TelegramClient, events, Button
+from telethon import TelegramClient, events
 from decouple import config
 
 logging.basicConfig(
@@ -21,79 +7,54 @@ logging.basicConfig(
 )
 log = logging.getLogger("ChannelAutoPost")
 
-# start the bot
-log.info("Starting...")
+# Memulai bot
+log.info("Memulai...")
 try:
     apiid = config("APP_ID", cast=int)
     apihash = config("API_HASH")
     bottoken = config("BOT_TOKEN")
-    frm = config("FROM_CHANNEL", cast=lambda x: [int(_) for _ in x.split(" ")])
     tochnls = config("TO_CHANNEL", cast=lambda x: [int(_) for _ in x.split(" ")])
     datgbot = TelegramClient(None, apiid, apihash).start(bot_token=bottoken)
 except Exception as exc:
-    log.error("Environment vars are missing! Kindly recheck.")
-    log.info("Bot is quiting...")
+    log.error("Variabel lingkungan hilang! Silakan periksa kembali.")
+    log.info("Bot keluar...")
     log.error(exc)
     exit()
 
 
-@datgbot.on(events.NewMessage(pattern="/start"))
+@datgbot.on(events.NewMessage(incoming=True, chats=datgbot))
 async def _(event):
-    await event.reply(
-        f"Hi `{event.sender.first_name}`!\n\nI am a channel auto-post bot!! Read /help to know more!\n\nI can be used in only two channels (one user) at a time. Kindly deploy your own bot.\n\n[More bots](https://t.me/its_xditya)..",
-        buttons=[
-            Button.url("Repo", url="https://github.com/xditya/ChannelAutoForwarder"),
-            Button.url("Dev", url="https://xditya.me"),
-        ],
-        link_preview=False,
-    )
-
-
-@datgbot.on(events.NewMessage(pattern="/help"))
-async def helpp(event):
-    await event.reply(
-        "**Help**\n\nThis bot will send all new posts in one channel to the other channel. (without forwarded tag)!\nIt can be used only in two channels at a time, so kindly deploy your own bot from [here](https://github.com/xditya/ChannelAutoForwarder).\n\nAdd me to both the channels and make me an admin in both, and all new messages would be autoposted on the linked channel!!\n\nLiked the bot? Drop a ♥ to @xditya_Bot :)"
-    )
-
-
-@datgbot.on(events.NewMessage(incoming=True, chats=frm))
-async def _(event):
-    for tochnl in tochnls:
-        try:
-            if event.poll:
-                return
-            if event.photo:
-                photo = event.media.photo
-                await datgbot.send_file(
-                    tochnl, photo, caption=event.text, link_preview=False
-                )
-            elif event.media:
-                try:
-                    if event.media.webpage:
-                        await datgbot.send_message(
-                            tochnl, event.text, link_preview=False
-                        )
-                except Exception:
-                    media = event.media.document
-                    await datgbot.send_file(
-                        tochnl, media, caption=event.text, link_preview=False
-                    )
-                finally:
+    if event.is_private:  # Periksa apakah pesan berasal dari obrolan pribadi
+        for tochnl in tochnls:
+            try:
+                if event.poll:
                     return
-            else:
-                await datgbot.send_message(tochnl, event.text, link_preview=False)
-        except Exception as exc:
-            log.error(
-                "TO_CHANNEL ID is wrong or I can't send messages there (make me admin).\nTraceback:\n%s",
-                exc,
-            )
+                if event.photo:
+                    photo = event.media.photo
+                    await datgbot.send_file(
+                        tochnl, photo, caption=event.text, link_preview=False
+                    )
+                elif event.media:
+                    try:
+                        if event.media.webpage:
+                            await datgbot.send_message(
+                                tochnl, event.text, link_preview=False
+                            )
+                    except Exception:
+                        media = event.media.document
+                        await datgbot.send_file(
+                            tochnl, media, caption=event.text, link_preview=False
+                        )
+                    finally:
+                        return
+                else:
+                    await datgbot.send_message(tochnl, event.text, link_preview=False)
+            except Exception as exc:
+                log.error(
+                    "ID TO_CHANNEL salah atau saya tidak dapat mengirim pesan di sana (jadikan saya admin).\nTraceback:\n%s",
+                    exc,
+                )
 
-@datgbot.on(events.NewMessage(incoming=True, chats=datgbot))  # Modify chats= 
-async def _(event):
-    if event.is_private:  # Check if message is from a private chat
-        await datgbot.send_message(tochnls, event.message)
-
-
-log.info("Bot has started.")
-log.info("Do visit https://xditya.me !")
+log.info("Bot telah dimulai.")
+log.info("Kunjungi https://xditya.me !")
 datgbot.run_until_disconnected()
